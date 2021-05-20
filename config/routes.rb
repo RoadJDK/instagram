22aab1389d63ed385740d0fcb977d2d8dc19f31c
RoadJDK <mtimo.official@gmail.com>
2021-05-20 15:32:40 +0200

    clean

--- config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  devise_for :users
-  root "login#index"
-  
-  get "/login", to: "login#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
