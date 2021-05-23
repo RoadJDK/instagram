930f0a9712004247d45969bd941a0b1ab51c9e9a
RoadJDK <mtimo.official@gmail.com>
2021-05-23 11:38:25 +0200

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'pages#home'
+  devise_for :users
+  root to: 'pages#home'
 
   get 'pages/home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
