dc2b5dfa299dd2ce9c9b37f1f6e28a9871712894
RoadJDK <mtimo.official@gmail.com>
2021-05-23 12:10:36 +0200

    edit migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates_length_of :name, maximum: 50, allow_blank: false
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
