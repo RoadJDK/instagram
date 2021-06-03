8b447ad950d81b2ac55bc2583d5207d086fab4f2
RoadJDK <mtimo.official@gmail.com>
2021-06-03 14:08:26 +0200

    AB151-05 done

--- Gemfile
+++ Gemfile
@@ -56,3 +56,9 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'devise'
+
+gem 'carrierwave'
+
+gem 'cloudinary'
+
+gem 'figaro'
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,12 +3,14 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-import "bootstrap"
+import Rails from "@rails/ujs";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
+import "bootstrap";
 
-global.toastr = require("toastr")
+import "@fortawesome/fontawesome-free";
 
-Rails.start()
-ActiveStorage.start()
+global.toastr = require("toastr");
+
+Rails.start();
+ActiveStorage.start();
